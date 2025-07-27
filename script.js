8c2a14765f08ef5fbbfb24da08d661e0d498a130
Victor <109152202+VictorWqzx@users.noreply.github.com>
2025-07-27 10:29:05 +0300

    FIX

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Выбираем все блоки с товарами, за которыми будем наблюдать
+    
     const productItems = document.querySelectorAll('.product-item');
 
-    // Проверяем, есть ли на странице такие блоки
+    
     if (productItems.length > 0) {
         
         // Создаем "наблюдатель"
         const observer = new IntersectionObserver((entries, observer) => {
             entries.forEach(entry => {
-                // Если элемент появился в области видимости
+                
                 if (entry.isIntersecting) {
                     // Добавляем ему класс для запуска анимации
                     entry.target.classList.add('is-visible');
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             });
         }, {
-            threshold: 0.1 // Анимация начнется, когда видно хотя бы 10% элемента
+            threshold: 0.1 
         });
 
         // "Просим" наблюдатель следить за каждым блоком товара
